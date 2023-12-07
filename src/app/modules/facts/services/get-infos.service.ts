90a8d7e0fdb17d7ad2ed46f1398a2dfe2920fc92
Volodia Fernandez <vfz1204@gmail.com>
2023-12-07 20:04:15 +0100

    Fix : Changed apiUrl

--- src/app/modules/facts/services/get-infos.service.ts
+++ src/app/modules/facts/services/get-infos.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 export interface IFact {
   title: string;
@@ -14,12 +15,8 @@ export class GetInfosService {
   #http = inject(HttpClient);
 
 
-  apiUrl = 'localhost:3000/api';
-
-
-
   getInfos(): Observable<IFact> {
-    return this.#http.get<IFact>(apiUrl).pipe(
+    return this.#http.get<IFact>(environment.API_URL).pipe(
       map((result) => {
         return result;
       })
