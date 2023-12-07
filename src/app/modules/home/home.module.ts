335a9756953adc08dc0ec99bc74c4fb93ac6355b
Volodia Fernandez <vfz1204@gmail.com>
2023-12-07 22:14:00 +0100

    Feat: Added translation config

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SharedModule } from '../../shared/shared.module';
+import { TranslocoRootModule } from 'src/app/transloco-root.module';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, HomeRoutingModule, SharedModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, TranslocoRootModule],
 })
 export class HomeModule {}
