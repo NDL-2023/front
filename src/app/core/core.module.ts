5e676108efad10edcde1c233569b5eecaa89790f
Volodia Fernandez <vfz1204@gmail.com>
2023-12-07 23:40:09 +0100

    Fix: Added translation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -11,6 +11,7 @@ import { BaseAppComponent } from './components/base-app/base-app.component';
 import { ModalInjectorComponent } from './components/modal-injector/modal-injector.component';
 import { ToastComponent } from './components/toast/toast.component';
 import { BaseModalComponent } from './components/base-modal/base-modal.component';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import { BaseModalComponent } from './components/base-modal/base-modal.component
     ModalInjectorComponent,
     BaseModalComponent,
   ],
-  imports: [CommonModule, HttpClientModule, RouterModule],
+  imports: [CommonModule, HttpClientModule, RouterModule, TranslocoModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
