e4cb4e1736254fc3b30b222d78f48bab9515eae2
skynat74 <skynat74@gmail.com>
2023-12-08 06:30:30 +0100

    Fix langages + german and swedish

--- src/app/core/components/nav-bar/nav-bar.component.ts
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -9,7 +9,9 @@ import { BaseAppComponent } from '../base-app/base-app.component';
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent extends BaseAppComponent {
-  currentLocale = 'fr';
+  ind = 0;
+  availableLangs: string[] = ['fr', 'en', 'sv', 'de'];
+  currentLocale = this.availableLangs[this.ind];
   //isConnected = false;
   //use AuthentificationService to get the current user
   phoneMenuOpen = false;
@@ -22,7 +24,12 @@ export class NavBarComponent extends BaseAppComponent {
   }
 
   switchLanguage() {
-    this.currentLocale = this.currentLocale == 'fr' ? 'en' : 'fr';
+    if (this.ind < this.availableLangs.length -1) {
+      this.ind += 1;
+    } else {
+      this.ind = 0;
+    }
+    this.currentLocale = this.availableLangs[this.ind];
     this.translocoService.setActiveLang(this.currentLocale);
   }
 }
