de154b76dc7b0a87300743fb7e71ef7fa14720c5
Hugo REY <hrey222@gmail.com>
2023-12-08 06:09:08 +0100

    feat: connect topic to back

--- src/app/modules/feed/services/topic.service.ts
+++ src/app/modules/feed/services/topic.service.ts
@@ -10,15 +10,24 @@ import { environment } from '../../../../environments/environment';
 export class TopicService {
   #http = inject(HttpClient);
 
-  getTopic(id: number): Observable<Topic> {
+  getTopic(id: number): Observable<any> {
     return this.#http
-      .get<Topic>(environment.API_URL + '/topic/' + id)
+      .get<any>(environment.API_URL + '/topic/?id=' + id)
       .pipe(take(1));
   }
 
   createTopic(topicDto: TopicDto): Observable<Topic> {
     return this.#http
       .post<Topic>(environment.API_URL + '/topic/create', topicDto)
+      .pipe(take(1));
+  }
+
+  replyTopic(id: number, content: string): Observable<Topic> {
+    return this.#http
+      .post<Topic>(environment.API_URL + '/topic/reply', {
+        id: id,
+        content: content,
+      })
       .pipe(take(1));
   }
 }
